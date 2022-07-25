f913e83aa34a421d649ba70eaa0d1e2e9536ea66
fasetbook <107675067+fasetbook@users.noreply.github.com>
2022-07-25 07:14:39 +0000

    Update good.bat

--- good.bat
+++ good.bat
@@ -1,6 +1,14 @@
+:one
 echo hello,I'm jack
 set aa=ticing aatuhfr FHIEH efcjsijhf fruh vhihwe bfrg
 for %%i in (%aa%) do md %%i
 del *.log
 cls
 pause >nul
+
+:two
+set /p URL=
+for /F "tokens=* delims=*" %%i in ('dir /B *.txt') do (
+  type %%i >>%URL%
+)
+cls
